fa6a2b0a6ce1507558bfe60558eb57e31234707a
BigStr <stkgud@gmail.com>
2016-09-21 16:21:38 +0800

    add-baidubaike_apider

---
+++ baike_spider/html_outputer.py
@@ -0,0 +1,40 @@
+# coding utf-8
+
+'''
+Created on 2016��7��16��
+
+@author: sktgu
+'''
+
+
+class HtmlOutputer(object):
+
+    def __init__(self):
+        self.datas = []
+
+
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        fout = open('oput.html', 'w')
+        fout.write("<html><meta charset=\"utf-8\" />")
+        fout.write("<body>")
+        fout.write("<table>")
+
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+        fout.write("</table>")
+        fout.write("</body>")
+        fout.write("</html>")
+        fout.close()
+    
+
+
+
